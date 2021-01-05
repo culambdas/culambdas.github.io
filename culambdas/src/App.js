0decaa9265e26b1564c203751d0629331aefe3f8
Jongwan Kim <jongwankim19@gmail.com>
2021-01-06 01:10:35 +0900

    hero page half done

--- culambdas/src/App.js
+++ culambdas/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Hero = styled.section`
+  width : 100vw;
+  height : 100vh;
+
+  display : flex;
+  flex-direction : column;
+  justify-content : center;
+  align-items : center;
+
+  background-image: url('/pics/smoke.mp4');
+  background-size : cover;
+  background-position : center center;
+  background-repeat : no-repeat;
+  background-attachment : fixed;
+`;
+
+const HeroInner = styled.h1`
+  color: white;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 10em;
+`;
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <Hero>
+        <HeroInner className="fade-in">
+          &Lambda; &Phi; &Epsilon;
+        </HeroInner>
+        
+        
+      </Hero>
+    );
+  }
 }
 
 export default App;
