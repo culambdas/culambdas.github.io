dce075232bcd2288730898a59fa567c3d3c6bb64
Samuel Lye <sl2982@cornell.edu>
2021-09-10 02:08:16 -0400

    close navbar on click

--- culambdas/src/Components/Navbar/Navbar.js
+++ culambdas/src/Components/Navbar/Navbar.js
@@ -1,9 +1,9 @@
-import "./navbar.css";
-import { Component } from "react";
-import Crest from "../../assets/images/Crest.png";
-import { Link } from "react-router-dom";
-import { VscMenu } from "react-icons/vsc";
-import { AiOutlineClose } from "react-icons/ai";
+import './navbar.css';
+import { Component } from 'react';
+import Crest from '../../assets/images/Crest.png';
+import { Link } from 'react-router-dom';
+import { VscMenu } from 'react-icons/vsc';
+import { AiOutlineClose } from 'react-icons/ai';
 
 export default class Navbar extends Component {
   constructor(props) {
@@ -13,23 +13,23 @@ export default class Navbar extends Component {
     this.closeNav = this.closeNav.bind(this);
   }
   openNav() {
-    document.body.style.overflow = "hidden"; // firefox, chrome
-    document.body.scroll = "no"; // ie only
-    document.getElementById("navOverlay").style.width = "100%";
+    document.body.style.overflow = 'hidden'; // firefox, chrome
+    document.body.scroll = 'no'; // ie only
+    document.getElementById('navOverlay').style.width = '100%';
   }
 
   closeNav() {
-    document.body.style.overflow = ""; // firefox, chrome
-    document.body.scroll = "yes"; // ie only
-    document.getElementById("navOverlay").style.width = "0%";
+    document.body.style.overflow = ''; // firefox, chrome
+    document.body.scroll = 'yes'; // ie only
+    document.getElementById('navOverlay').style.width = '0%';
   }
 
   componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
+    window.addEventListener('scroll', this.handleScroll);
   }
 
   componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
+    window.removeEventListener('scroll', this.handleScroll);
   }
 
   handleScroll = () => {
@@ -37,21 +37,21 @@ export default class Navbar extends Component {
     const viewportHeight = document.documentElement.clientHeight;
     if (viewportWidth > 1008) {
       if (window.scrollY > viewportHeight) {
-        document.querySelector(".nav-toggle").className = "nav-toggle scroll";
+        document.querySelector('.nav-toggle').className = 'nav-toggle scroll';
       } else {
-        document.querySelector(".nav-toggle").className = "nav-toggle";
+        document.querySelector('.nav-toggle').className = 'nav-toggle';
       }
     } else if (512 <= viewportWidth <= 1008) {
       if (window.scrollY > viewportHeight * 0.7) {
-        document.querySelector(".nav-toggle").className = "nav-toggle scroll";
+        document.querySelector('.nav-toggle').className = 'nav-toggle scroll';
       } else {
-        document.querySelector(".nav-toggle").className = "nav-toggle";
+        document.querySelector('.nav-toggle').className = 'nav-toggle';
       }
     } else {
       if (window.scrollY > viewportHeight * 0.5) {
-        document.querySelector(".nav-toggle").className = "nav-toggle scroll";
+        document.querySelector('.nav-toggle').className = 'nav-toggle scroll';
       } else {
-        document.querySelector(".nav-toggle").className = "nav-toggle";
+        document.querySelector('.nav-toggle').className = 'nav-toggle';
       }
     }
   };
@@ -73,22 +73,22 @@ export default class Navbar extends Component {
           <AiOutlineClose className="closebtn" onClick={this.closeNav} />
           {/* <!-- Overlay content --> */}
           <div className="nav-menu-content">
-            <Link as={Link} to="/">
+            <Link as={Link} to="/" onClick={this.closeNav}>
               Home
             </Link>
-            <Link as={Link} to="about">
+            <Link as={Link} to="about" onClick={this.closeNav}>
               About
             </Link>
-            <Link as={Link} to="leadership">
+            <Link as={Link} to="leadership" onClick={this.closeNav}>
               Leadership
             </Link>
-            <Link as={Link} to="active-house">
+            <Link as={Link} to="active-house" onClick={this.closeNav}>
               Active House
             </Link>
-            <Link as={Link} to="roster">
+            <Link as={Link} to="roster" onClick={this.closeNav}>
               Roster
             </Link>
-            <Link as={Link} to="rush">
+            <Link as={Link} to="rush" onClick={this.closeNav}>
               Rush
             </Link>
           </div>
