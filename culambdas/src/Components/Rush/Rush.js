77229f7da2d7668a7fac2cd63ed57dde271eb70e
Samuel Lye <sl2982@cornell.edu>
2021-09-10 01:50:02 -0400

    rush flip cards

--- culambdas/src/Components/Rush/Rush.js
+++ culambdas/src/Components/Rush/Rush.js
@@ -1,26 +1,27 @@
 import '../Home/home.css';
+import './rush.css';
 import smoke from '../../assets/images/smoke.mp4';
+// import fa21rushcover from '../../assets/images/fa21rushcover.png';
+import fa21front from '../../assets/images/rush/front.png';
+import fa21back from '../../assets/images/rush/back.png';
 
 const Rush = () => {
   return (
-    <>
-      <section>
-        <video className="hero" autoPlay loop muted>
-          <source src={smoke} type="video/mp4" />
-        </video>
-        <div className="splash-text fade-in">
-          <span className="lfe-splash-text">Fall 2021 Rush</span>
-          <br />
-          <span className="cornell-university-splash-text">
-            
-          </span>
-          <br />
-          <span className="est-splash-text">Coming Soon...</span>
+    <section>
+      <video className="hero" autoPlay loop muted>
+        <source src={smoke} type="video/mp4" />
+      </video>
+      <div className="flip-container fade-in">
+        <div className="flipper">
+          <div className="front">
+            <img src={fa21front} alt="rush-cover" width="100%" />
+          </div>
+          <div className="back">
+            <img src={fa21back} alt="rush-schedule" width="100%" />
+          </div>
         </div>
-      </section>
-
-      <section></section>
-    </>
+      </div>
+    </section>
   );
 };
 
